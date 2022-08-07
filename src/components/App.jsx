bdcfe9253e32ef7cfe1bda3e18d0dfb6b422db25
illya bondar <1816579@gmail.com>
2022-08-07 17:23:30 +0300

    initial files

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: '#010101',
+        backgroundColor: 'var(--back-color-2)',
+      }}
+    >
+      <h1>React App</h1>
+    </div>
+  );
+};
