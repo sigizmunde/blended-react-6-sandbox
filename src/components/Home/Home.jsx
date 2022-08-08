85e53ec13e016e4c85c7e6c0d3176d3d2d185c30
illya bondar <1816579@gmail.com>
2022-08-08 22:50:58 +0300

    fix: empty list doesn't generate table

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,45 +10,47 @@ const Home = () => {
 
   return (
     <div>
-      <Table>
-        <Thead>
-          <tr>
-            <th>No</th>
-            <th>Avatar</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Status</th>
-            <th>Option</th>
-          </tr>
-        </Thead>
-        <tbody>
-          {users.map(({ id, name, avatar, age, isOnline }, index) => {
-            return (
-              <tr key={id}>
-                <td>{index + 1}</td>
-                <td>
-                  <Avatar name={avatar} size={42} round={true} />
-                </td>
-                <td>{name}</td>
-                <td>{age}</td>
-                <td>
-                  <Status onClick={() => dispatch(toggleStatus(id))}>
-                    {isOnline === 'yes' ? 'online' : 'offline'}
-                  </Status>
-                </td>
-                <td>
-                  <Button
-                    type="button"
-                    onClick={() => dispatch(removeUser(id))}
-                  >
-                    delete
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      {users.length > 0 && (
+        <Table>
+          <Thead>
+            <tr>
+              <th>No</th>
+              <th>Avatar</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Status</th>
+              <th>Option</th>
+            </tr>
+          </Thead>
+          <tbody>
+            {users.map(({ id, name, avatar, age, isOnline }, index) => {
+              return (
+                <tr key={id}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <Avatar name={avatar} size={42} round={true} />
+                  </td>
+                  <td>{name}</td>
+                  <td>{age}</td>
+                  <td>
+                    <Status onClick={() => dispatch(toggleStatus(id))}>
+                      {isOnline === 'yes' ? 'online' : 'offline'}
+                    </Status>
+                  </td>
+                  <td>
+                    <Button
+                      type="button"
+                      onClick={() => dispatch(removeUser(id))}
+                    >
+                      delete
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
     </div>
   );
 };
